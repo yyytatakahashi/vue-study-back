28b8e7b30c8d843333ac27053beeef88f5e04cb7
yyytatakahashi <yuta01072002@icloud.com>
2025-07-27 19:34:59 +0900

    Fix: correct JAVA_HOME path for Render

--- .render/build.sh
+++ .render/build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-export JAVA_HOME=/opt/render/project/.java/openjdk
+# 正しいJAVA_HOMEを明示的に指定
+export JAVA_HOME=/opt/render/project/java/openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 
 ./mvnw clean package
+
 
